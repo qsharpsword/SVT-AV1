3d45d2f7246e2177d0fa73cbdd5ad4021e3f71cf
hguermaz <hassen.guermazi@intel.com>
2020-07-21 14:24:32 -0700

    back to 16

--- Source/Lib/Encoder/Codec/EbFullLoop.c
+++ Source/Lib/Encoder/Codec/EbFullLoop.c
@@ -1822,12 +1822,11 @@ int32_t av1_quantize_inv_quantize(
             // effective.
             // TODO(any): Experiment with variance and mean based thresholds
             const int is_small_residual = 0;
-            //    ((uint64_t)block_mse_q8 <=
-            //    (uint64_t) 16 /*coeff_opt_dist_thresholds[5] */* qstep * qstep);
-            //// Turn OFF RDOQ if large resudual
+                ((uint64_t)block_mse_q8 <=
+                (uint64_t) 16 /*coeff_opt_dist_thresholds[5] */* qstep * qstep);
+            // Turn OFF RDOQ if large resudual
             perform_rdoq = is_small_residual;
         }
-        perform_rdoq = 0;
 #endif
     }
 #else
