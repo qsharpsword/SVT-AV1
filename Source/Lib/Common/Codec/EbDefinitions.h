ca766f0beda0e2e7cc166479ef11b76efab05174
hguermaz <hassen.guermazi@intel.com>
2020-08-04 19:32:32 -0700

    SUPER_FAST_MRP_OFF

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -681,8 +681,8 @@ extern "C" {
 #define SUPER_FAST_SHUT_RDOQ          0 
 #define SUPER_FAST_SHUT_SPATIAL_SSE   0
 #define SUPER_FAST_SHUT_TXT           0
-#define SUPER_FAST_SHUT_TXS           1
-#define SUPER_FAST_MRP_OFF            0
+#define SUPER_FAST_SHUT_TXS           0
+#define SUPER_FAST_MRP_OFF            1
 #define SUPER_FAST_HP_OFF             0
 #define SUPER_FAST_WARP_OFF           0
 #define SUPER_FAST_PRED_ONLY_B_SLICE  0
