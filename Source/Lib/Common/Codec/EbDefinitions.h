b5a6bc6fecc64132160cd1441e5a38cd3f6fda5f
hguermaz <hassen.guermazi@intel.com>
2020-08-04 19:27:40 -0700

    SUPER_FAST_SHUT_SPATIAL_SSE

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -678,8 +678,8 @@ extern "C" {
 
 /******************************/
 #define SUPER_FAST_SHUT_PME           0
-#define SUPER_FAST_SHUT_RDOQ          1 
-#define SUPER_FAST_SHUT_SPATIAL_SSE   0
+#define SUPER_FAST_SHUT_RDOQ          0 
+#define SUPER_FAST_SHUT_SPATIAL_SSE   1
 #define SUPER_FAST_SHUT_TXT           0
 #define SUPER_FAST_SHUT_TXS           0
 #define SUPER_FAST_MRP_OFF            0
