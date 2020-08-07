d1a40af2d980c1f86767b3135f00a745295e941b
hguermaz <hassen.guermazi@intel.com>
2020-08-06 23:25:46 -0700

    child 20

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -11444,7 +11444,7 @@ static void perform_pred_depth_refinement(SequenceControlSet *scs_ptr, PictureCo
                                 (int64_t)(context_ptr->md_local_blk_unit[blk_geom->sqi_mds].default_cost);
                         }
                     }
-                    if (child_to_current_deviation > 15)
+                    if (child_to_current_deviation > 20)
                         e_depth = 0;
 #endif
                     if (e_depth != 0)
